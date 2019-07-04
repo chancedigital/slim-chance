b0e9d9ad238342455694427b306a7791b5f32157
Chance Strickland <hi@chancedigital.io>
2019-07-04 12:43:12 -0700

    add feat images to post preview

--- templates/parts/layout/blocks/post-preview.php
+++ templates/parts/layout/blocks/post-preview.php
@@ -1,6 +1,17 @@
+<?php
+$feat_image = get_the_post_thumbnail( $post->ID, 'medium', [ 'class' => 'post-preview__img' ] )
+?>
+
 <article <?php post_class( 'post-preview' ) ?>>
 	<header class="post-preview__header">
 		<?php
+		if ( $feat_image ) :
+			?>
+			<div class="post-preview__img-wrapper">
+				<?php echo $feat_image ?>
+			</div>
+			<?php
+		endif;
 		the_title(
 			'<a class="post-preview__link" href="' . get_the_permalink() . '" rel="bookmark"><h3 class="post-preview__title">',
 			'</h3></a>'
