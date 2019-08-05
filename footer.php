64812ac0909e7a4e0c695f88d23df15df9ce993d
Chance Strickland <hi@chancedigital.io>
2019-08-05 08:01:49 -0700

    make footer info translatable

--- footer.php
+++ footer.php
@@ -15,32 +15,41 @@ use ChanceDigital\Slim_Chance\Nav\Walker_Nav_Menu_Bem;
 
 <footer class="site-footer">
 	<div class="site-footer__wrapper">
+		<?php
+		wp_nav_menu(
+			[
+				'menu_class'      => 'social-nav__menu',
+				'container'       => 'nav',
+				'container_class' => 'social-nav site-footer__social-nav',
+				'theme_location'  => 'social',
+				'link_before'     => '<span class="screen-reader-text">',
+				'link_after'      => '</span">',
+				'walker'          => new Walker_Nav_Menu_Bem(),
+			]
+		);
+		wp_nav_menu(
+			[
+				'menu_class'      => 'footer-nav__menu',
+				'container'       => 'nav',
+				'container_class' => 'footer-nav site-footer__nav',
+				'theme_location'  => 'footer-navigation',
+				'walker'          => new Walker_Nav_Menu_Bem(),
+			]
+		);
+		?>
+		<nav class="site-footer__copyright" aria-label="<?php esc_attr_e( 'Additional Information', 'slim-chance' ) ?>">
+			<a href="/privacy-policy/"><?php esc_html_e( 'Privacy Policy', 'slim-chance' ) ?></a>.
 			<?php
-			wp_nav_menu(
-				[
-					'menu_class'      => 'social-nav__menu',
-					'container'       => 'nav',
-					'container_class' => 'social-nav site-footer__social-nav',
-					'theme_location'  => 'social',
-					'link_before'     => '<span class="screen-reader-text">',
-					'link_after'      => '</span">',
-					'walker'          => new Walker_Nav_Menu_Bem(),
-				]
-			);
-			wp_nav_menu(
-				[
-					'menu_class'      => 'footer-nav__menu',
-					'container'       => 'nav',
-					'container_class' => 'footer-nav site-footer__nav',
-					'theme_location'  => 'footer-navigation',
-					'walker'          => new Walker_Nav_Menu_Bem(),
-				]
-			);
+			echo esc_html( sprintf(
+				/* translators: 1. Copyright symbol; 2. Current year */
+				__( '%1$s %2$s All Rights Reserved', 'slim-chance' ),
+				'&copy;',
+				date( 'Y' )
+			) );
 			?>
-		<nav></nav>
-		<div class="site-footer__copyright">
-			<a href="/privacy-policy/">Privacy Policy</a>. &copy; <?php echo date( 'Y' ) ?> All Rights Reserved.<br /><span style="font-weight: 300;color: rgba(255,255,255,0.5);">Website by <a style="color: rgba(255,255,255,0.5);" href="https://chancedigital.io" target="_blank" rel="nofollow noopener noreferrer">Chance Digital</a></span>
-		</div>
+			<br />
+			<span style="font-weight: 300;color: rgba(255,255,255,0.5);">Website by <a style="color: rgba(255,255,255,0.5);" href="https://chancedigital.io" target="_blank" rel="nofollow noopener noreferrer">Chance Digital</a></span>
+		</nav>
 	</div>
 </footer>
 
