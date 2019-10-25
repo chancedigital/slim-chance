7056613045d5bfe717ffe805182704029dc28ddf
Chance Strickland <hi@chancedigital.io>
2019-10-25 12:02:57 -0400

    fix block bug

--- templates/parts/templates/page-home/section-hero.php
+++ templates/parts/templates/page-home/section-hero.php
@@ -1,8 +1,11 @@
 <?php
+
 $page_header_background_image = get_field( 'page_header_background_image' );
 $page_header_content          = get_field( 'page_header_content' );
 $page_heading                 = $page_header_content['heading'];
 $page_subheading              = $page_header_content['subheading'];
+$button_label                 = $page_header_content['button_label'];
+$button_type                  = $page_header_content['button_link_type'];
 ?>
 
 <section class="home-hero">
@@ -23,6 +26,31 @@ $page_subheading              = $page_header_content['subheading'];
 			<div class="home-hero__content">
 				<h1 class="home-hero__heading"><?php echo esc_html( $page_header_content['heading'] ) ?></h1>
 				<p class="home-hero__subheading"><?php echo esc_html( $page_header_content['subheading'] ) ?></p>
+
+				<?php
+				if ( $button_label && $button_type ) :
+					$href = $button_type === 'internal'
+						? $page_header_content['button_link_internal']
+						: $page_header_content['button_link_external'];
+
+					$target = $button_type === 'internal'
+						? '_self'
+						: '_blank';
+
+					$rel = $button_type === 'internal'
+						? ''
+						: 'noreferrer noopener';
+					?>
+
+					<a
+						class="button home-hero__button"
+						href="<?php echo esc_url( $href ) ?>"
+						target="<?php echo esc_attr( $target ) ?>"
+						rel="<?php echo esc_attr( $rel ) ?>"
+					>
+						<?php echo esc_html( $button_label ) ?>
+					</a>
+				<?php endif ?>
 			</div>
 
 		<?php endif; ?>
