d336f215478a8112442ea634def670787a2414b8
chancestrickland <chance.strickland@gmail.com>
2019-12-10 14:13:48 +0100

    rm decorator support

--- config/babel.config.js
+++ config/babel.config.js
@@ -3,11 +3,10 @@ module.exports = {
 	plugins: [
 		'@babel/plugin-transform-runtime',
 		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-decorators',
-		'@babel/plugin-proposal-dynamic-import',
 		'@babel/plugin-proposal-nullish-coalescing-operator',
 		'@babel/plugin-proposal-object-rest-spread',
 		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-syntax-dynamic-import',
 		'@babel/plugin-transform-react-jsx',
 		'lodash',
 	],
