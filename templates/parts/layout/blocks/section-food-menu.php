4a39554afa32ca944cbdc023823b589a426a5a39
chancestrickland <chance.strickland@gmail.com>
2019-12-10 16:42:34 +0100

    add pizza layout

--- templates/parts/layout/blocks/section-food-menu.php
+++ templates/parts/layout/blocks/section-food-menu.php
@@ -1,75 +1,61 @@
 <?php
-$food_menu = get_field( 'food_menu', 'option' );
-$ingr_list = get_field( 'ingredient_list', 'option' );
-?>
-<section class="section-food-menu" id="<?php echo esc_attr( get_sub_field( 'section_id' ) ?: uniqid() ) ?>">
-	<div class="section-food-menu__wrapper">
-		<header class="section-food-menu__header">
-			<h2 class="section-food-menu__heading">
-				You Can Get With <strong>This</strong>
-			</h2>
-			<p class="section-food-menu__subheading">S+H Signature Artisan Pizzas</p>
-			<?php if ( $food_menu ) : ?>
-				<a
-					href="<?php echo esc_url( $food_menu['url'] ) ?>"
-					class="section-food-menu__button button"
-					target="_blank"
-					rel="noreferrer noopener"
-				>
-					Click to Print
-				</a>
-			<?php endif; ?>
-		</header>
+$cat_slug = 'food_category';
+$_terms   = get_terms( [ $cat_slug ] );
+//var_dump( $_terms );
 
-		<?php
-		$menu_query = new WP_Query(
-			[
-				'post_type'      => 'food',
-				'posts_per_page' => 50,
-			]
-		);
-		if ( $menu_query->have_posts() ) :
-			?>
-			<ul class="section-food-menu__menu">
+if ( ! empty( $_terms ) ) :
+	?>
+	<section class="section-food-menu" id="<?php echo esc_attr( get_sub_field( 'section_id' ) ?: uniqid() ) ?>">
+
+		<?php foreach ( $_terms as $_term ) : ?>
+
+			<div class="section-food-menu__wrapper">
+				<h2 class="section-food-menu__heading"><?php echo esc_html( $_term->name ) ?></h2>
+
 				<?php
-				while ( $menu_query->have_posts() ) :
-					$menu_query->the_post();
+				$menu_query = new WP_Query(
+					[
+						'post_type'      => 'food',
+						'posts_per_page' => 50,
+						'tax_query' => [
+							[
+								'taxonomy' => $cat_slug,
+								'field'    => 'slug',
+								'terms'    => $_term->slug,
+							],
+						],
+					]
+				);
+				if ( $menu_query->have_posts() ) :
 					?>
-
-					<li class="section-food-menu__item">
+					<ul class="section-food-menu__menu">
 						<?php
-						the_title(
-							'<h3 class="section-food-menu__item-heading">',
-							'</h3>'
-						);
-						?>
-						<div class="section-food-menu__item-description">
-							<?php echo wp_kses_post( get_field( 'description' ) ); ?>
-						</div>
-					</li>
+						while ( $menu_query->have_posts() ) :
+							$menu_query->the_post();
+							?>
 
-				<?php endwhile; ?>
-			</ul>
-			<?php
-		endif;
-		wp_reset_postdata();
-		?>
+							<li class="section-food-menu__item">
+								<?php
+								the_title(
+									'<h3 class="section-food-menu__item-heading">',
+									'</h3>'
+								);
+								?>
+								<div class="section-food-menu__item-description">
+									<?php echo wp_kses_post( get_field( 'description' ) ); ?>
+								</div>
+							</li>
 
-		<footer class="section-food-menu__footer">
-			<h2 class="section-food-menu__heading">
-				<em>Or</em> You Can Get With <strong>That</strong>
-			</h2>
-			<p class="section-food-menu__subheading">Build Your Own Pizza</p>
-			<?php if ( $ingr_list ) : ?>
-				<a
-					href="<?php echo esc_url( $ingr_list['url'] ) ?>"
-					class="section-food-menu__button button"
-					target="_blank"
-					rel="noreferrer noopener"
-				>
-					See All Ingredients
-				</a>
-			<?php endif; ?>
-		</footer>
-	</div>
-</section>
+						<?php endwhile; ?>
+					</ul>
+					<?php
+				endif;
+				wp_reset_postdata();
+				?>
+			</div>
+
+		<?php endforeach; ?>
+
+	</section>
+
+<?php endif; ?>
