d6b78772e5e6890b3f1cc7b09d542c2db5e39555
chancestrickland <chance.strickland@gmail.com>
2019-12-10 15:27:55 +0100

    grid updates

--- templates/parts/layout/blocks/section-two-column.php
+++ templates/parts/layout/blocks/section-two-column.php
@@ -3,10 +3,10 @@
 		<?php if ( get_sub_field( 'section_heading' ) ) : ?>
 			<h2 class="section-two-column__heading"><?php echo esc_html( get_sub_field( 'section_heading' ) ) ?></h2>
 		<?php endif; ?>
-		<div class="section-two-column__col">
+		<div class="section-two-column__col section-two-column__col--1">
 			<?php echo wp_kses_post( get_sub_field( 'left_content' ) ) ?>
 		</div>
-		<div class="section-two-column__col">
+		<div class="section-two-column__col section-two-column__col--2">
 			<?php echo wp_kses_post( get_sub_field( 'right_content' ) ) ?>
 		</div>
 	</div>
