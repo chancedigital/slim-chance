1809860ee74bee4668ed7a8c7a5cde9fda221ca9
chancestrickland <chance.strickland@gmail.com>
2019-12-10 12:52:15 +0100

    update deps

--- config/babel.config.js
+++ config/babel.config.js
@@ -3,8 +3,12 @@ module.exports = {
 	plugins: [
 		'@babel/plugin-transform-runtime',
 		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-transform-react-jsx',
+		'@babel/plugin-proposal-decorators',
+		'@babel/plugin-proposal-dynamic-import',
+		'@babel/plugin-proposal-nullish-coalescing-operator',
 		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/plugin-proposal-optional-chaining',
+		'@babel/plugin-transform-react-jsx',
 		'lodash',
 	],
 };
