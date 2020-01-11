e8b32b38050729f8cae96c21192029eb1e72b9e7
Chance Strickland <hi@chancedigital.io>
2020-01-11 12:46:54 -0800

    cleanup

--- config/babel.config.js
+++ config/babel.config.js
@@ -2,9 +2,11 @@ module.exports = {
 	presets: [ '@babel/preset-env', '@babel/preset-react' ],
 	plugins: [
 		'@babel/plugin-transform-runtime',
-		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-transform-react-jsx',
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-nullish-coalescing-operator',
 		'@babel/plugin-proposal-object-rest-spread',
+		'@babel/plugin-proposal-optional-chaining',
 		'lodash',
 	],
 };
